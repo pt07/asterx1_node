91e83e046409eafbf888ef8d5bf727652ba9c6aa
Pier Tirindelli <pier.tirindelli@gmail.com>
2016-01-18 10:42:08 +0100

    updated package, cmakelist and added empty source files

---
+++ src/raw_receiver_main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main(int argc, char **argv)
+{
+    std::cout << "Ciao\n";
+
+
+    return 0;
+}
---
+++ src/raw_receiver_node.h
@@ -0,0 +1,16 @@
+
+#ifndef RAW_REC_NODE_H
+#define RAW_REC_NODE_H
+
+#include <ros/ros.h>
+
+
+class RawReceiverNode
+{
+
+};
+
+
+
+
+#endif

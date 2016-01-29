a1aec030c3fa6a4f3772abda914969789c365c38
Pier Tirindelli <pier.tirindelli@gmail.com>
2016-01-29 11:27:50 +0100

    trilateration_node solves position using trilateration library

--- src/trilateration_node.cpp
+++ src/trilateration_node.cpp
@@ -11,9 +11,36 @@ TrilaterationNode::TrilaterationNode():
 
 }
 
+
 TrilaterationNode::~TrilaterationNode() { }
+
 
 void TrilaterationNode::pseudorangeCallback(const asterx1_node::SatPrArray::ConstPtr &msg)
 {
+    std::cout << "Received " << msg->measurements.size() << " sat obs at " << msg->timestamp << "\n";
+
+
+    std::vector<SatelliteMeasurement> measurements;
+
+
+    for (int i = 0; i < msg->measurements.size(); ++i)
+    {
+        const asterx1_node::SatPr sat = msg->measurements[i];
+
+        SatelliteMeasurement m = {
+                Point<double>(sat.x, sat.y, sat.z),
+                sat.pseudorange
+        };
+
+        measurements.push_back(m);
+    }
+
+
+    Receiver estRec = tr.computePosition(measurements);
+
+    std::cout << " ---> Estimation:\t" << estRec.toString() << std::endl;
+
+    // Sets the guess for the next simulation in the actual position
+    tr.setInitialReceiverGuess(estRec);
 
 }
--- src/trilateration_node.h
+++ src/trilateration_node.h
@@ -10,7 +10,8 @@
 #include "asterx1_node/SatPr.h"
 #include "asterx1_node/SatPrArray.h"
 
-
+//TODO è meglio installare la libreria e poi fare un include normale?
+#include "../include/trilateration/src/trilateration.h"
 
 class TrilaterationNode
 {
@@ -19,7 +20,10 @@ public:
     ~TrilaterationNode();
     void pseudorangeCallback(const asterx1_node::SatPrArray::ConstPtr &msg);
 
+
 protected:
+    Trilateration tr;
+
     // ROS node handle
     ros::NodeHandle nh;
 
